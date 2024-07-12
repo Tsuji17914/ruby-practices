a9adb24c1ed83be7142c3e8255b78807054ce7b5
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-07-12 16:01:36 +0900

    2nd Change

--- bowling.rb
+++ bowling.rb
@@ -7,51 +7,22 @@ scores = score.split(',')
 shots = []
 
 scores.each do |s|
-  shots << if s == 'X'
-             10
-           else
-             s.to_i
-           end
-end
-
-frames = []
-i = 0
-
-while i < shots.size
-  if frames.size < 9
-    if shots[i] == 10
-      frames << [shots[i]]
-      i += 1
-    else
-      frames << [shots[i], shots[i + 1]]
-      i += 2
-    end
-  else
-    frames << shots[i..i + 2]
-    break
-  end
+  shots << (s == 'X' ? 10 : s.to_i)
 end
 
 point = 0
 i = 0
 
-frames.each_with_index do |frame, index|
-  if index < 9
-    if frame[0] == 10
-      next_shot1 = shots[i + 1]
-      next_shot2 = shots[i + 2]
-      point += 10 + next_shot1 + next_shot2
-      i += 1
-    elsif frame.sum == 10
-      next_shot = shots[i + 2]
-      point += 10 + next_shot
-      i += 2
-    else
-      point += frame.sum
-      i += 2
-    end
+10.times.sum do |f|
+  if shots[i] == 10
+    point += 10 + shots[(i + 1)..(i + 2)].sum
+    i += 1
+  elsif shots[i] + shots[i + 1] == 10
+    point += 10 + shots[i + 2]
+    i += 2
   else
-    point += frame.sum
+    point += shots[(i)..(i + 1)].sum
+    i += 2
   end
 end
 
