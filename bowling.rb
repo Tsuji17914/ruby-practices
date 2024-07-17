61934bb430d15cd803687eeb1b4ea4458f5c0ec7
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-07-17 11:22:58 +0900

    4th change

--- bowling.rb
+++ bowling.rb
@@ -12,18 +12,20 @@ end
 
 point = 0
 i = 0
+frame = 0
 
-10.times.sum do
+while frame < 10
   if shots[i] == 10
-    point += 10 + shots[(i + 1)..(i + 2)].sum
+    point +=  10 + shots[i + 1] + shots[i + 2]
     i += 1
   elsif shots[i] + shots[i + 1] == 10
     point += 10 + shots[i + 2]
     i += 2
   else
-    point += shots[(i)..(i + 1)].sum
+    point += shots[i] + shots[i + 1]
     i += 2
   end
+  frame += 1
 end
 
 puts point
