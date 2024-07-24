7d634c5444352d49f584b5b4d9b9fa9467b4909f
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-07-24 09:32:50 +0900

    add Enumerable#sum

--- bowling.rb
+++ bowling.rb
@@ -10,22 +10,20 @@ scores.each do |s|
   shots << (s == 'X' ? 10 : s.to_i)
 end
 
-point = 0
 i = 0
-frame = 0
 
-while frame < 10
+point = 10.times.sum do
   if shots[i] == 10
-    point +=  10 + shots[i + 1] + shots[i + 2]
+    frame =  10 + shots[i + 1] + shots[i + 2]
     i += 1
   elsif shots[i] + shots[i + 1] == 10
-    point += 10 + shots[i + 2]
+    frame = 10 + shots[i + 2]
     i += 2
   else
-    point += shots[i] + shots[i + 1]
+    frame = shots[i] + shots[i + 1]
     i += 2
   end
-  frame += 1
+  frame
 end
 
 puts point
