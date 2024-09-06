d0d87d7432d1bb7c3092da5c56a7b32f3242c16c
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-09-06 10:59:21 +0900

    -a and -r merge

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 COLUMNS = 3
 PADDING = 2
 
-def list_directories
-  Dir.entries('.').reject { |f| f.start_with?('.') }.sort
+def list_directories(show_all,reverse)
+  entries = Dir.entries('.').sort
+  entries.reject! { |f| f.start_with?('.') } unless show_all
+  entries.reverse! if reverse
+  entries
 end
 
 def slice_contents(current_directory, columns)
@@ -33,7 +38,13 @@ def display_contents(transposed_contents)
   end
 end
 
-current_directory = list_directories
+opt = OptionParser.new
+options = {}
+opt.on('-a') { options[:show_all] = true }
+opt.on('-r') { options[:reverse] = true }
+opt.parse!(ARGV)
+
+current_directory = list_directories(options[:show_all], options[:reverse])
 contents = slice_contents(current_directory, COLUMNS)
 formatted_contents = format_columns(contents)
 transposed_contents = formatted_contents.transpose
