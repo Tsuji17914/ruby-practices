c0df1221d0cf8122e45cd481720fbb211ba4e4c5
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-09-06 11:12:03 +0900

    merge all

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,15 +2,55 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 COLUMNS = 3
 PADDING = 2
 
-def list_directories(show_all,reverse)
+PERMISSION_MAP = {
+   7 => 'rwx',
+   6 => 'rw-',
+   5 => 'r-x',
+   4 => 'r--',
+   3 => '-wx',
+   2 => '-w-',
+   1 => '--x'
+ }.freeze
+
+ FILE_TYPE_MAP = {
+   'file' => '-',
+   'directory' => 'd',
+   'fifo' => 'p',
+   'characterSpecial' => 'c',
+   'blockSpecial' => 'b',
+   'link' => 'l',
+   'socket' => 's'
+ }.freeze
+
+ def permission_string(allow)
+  allow.chars.map { |i| PERMISSION_MAP[i.to_i] || '---' }.join
+end
+
+def list_directories(show_all,reverse,long_format)
   entries = Dir.entries('.').sort
   entries.reject! { |f| f.start_with?('.') } unless show_all
   entries.reverse! if reverse
-  entries
+  if long_format
+    entries.each do |filename|
+      file_stat = File.stat(filename)
+      file_type = FILE_TYPE_MAP[File.ftype(filename)] || '?'
+      permission = file_stat.mode.to_s(8).slice(-3, 3)
+      permission_str = permission_string(permission)
+      hard_link = file_stat.nlink
+      owner = Etc.getpwuid(file_stat.uid).name
+      group_owner = Etc.getgrgid(file_stat.gid).name
+      block_size = file_stat.size.to_s.rjust(4)
+      last_update_time = file_stat.mtime.strftime('%_m %_d %H:%M')
+      puts "#{file_type}#{permission_str} #{hard_link} #{owner} #{group_owner} #{block_size} #{last_update_time} #{filename}"
+    end
+  else
+    entries
+  end
 end
 
 def slice_contents(current_directory, columns)
@@ -42,11 +82,14 @@ opt = OptionParser.new
 options = {}
 opt.on('-a') { options[:show_all] = true }
 opt.on('-r') { options[:reverse] = true }
+opt.on('-l') { options[:long_format] = true }
 opt.parse!(ARGV)
 
-current_directory = list_directories(options[:show_all], options[:reverse])
-contents = slice_contents(current_directory, COLUMNS)
-formatted_contents = format_columns(contents)
-transposed_contents = formatted_contents.transpose
+current_directory = list_directories(options[:show_all], options[:reverse],options[:long_format])
+unless options[:long_format]
+  contents = slice_contents(current_directory, COLUMNS)
+  formatted_contents = format_columns(contents)
+  transposed_contents = formatted_contents.transpose
 
-display_contents(transposed_contents)
+  display_contents(transposed_contents)
+end
